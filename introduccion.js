3dd042a8172d5b3c1c423eb6341c2103e9884c70
casandra <angie200896@gmail.com>
2021-05-19 20:02:36 -0400

    INTRODUCCION p2

---
+++ introduccion.js
@@ -0,0 +1,7 @@
+//Cree una función llamada “greet” para poder saludar a una persona y además mostrarle su edad, es decir, la salida de la función debe ser algo parecido a “hello Josie, your age is 25”
+function greet(nombre, edad) {
+    console.log('hello ', nombre, 'your age is ', edad);
+}
+greet('Angie', 23);
+/*¿Qué tipo de retorno le asigno TypeScript a esta función?
+R. Dara un retorno de tipo string y number.*/ 
--- introduccion.ts
+++ introduccion.ts
@@ -1,6 +1,7 @@
-/*var nombre = 'casandra';
-nombre = 123; */
-
-//¿Por qué TypeScript no le permite realizar la acción anterior?
-//R. Porque en typescript al declarar la variable como un String no se la puede manejar como un number a diferencia de javascript
-
+//Cree una función llamada “greet” para poder saludar a una persona y además mostrarle su edad, es decir, la salida de la función debe ser algo parecido a “hello Josie, your age is 25”
+function greet (nombre, edad){
+    console.log('hello ', nombre, 'your age is ', edad)
+ }
+ greet('Angie',23);
+ /*¿Qué tipo de retorno le asigno TypeScript a esta función?
+ R. Dara un retorno de tipo string y number.*/
