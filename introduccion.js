2cd27eed5d4ca0cdb4fb5667a2745f0ea9fd1aba
casandra <angie200896@gmail.com>
2021-05-19 20:06:28 -0400

    INTRODUCCION p4

--- introduccion.js
+++ introduccion.js
@@ -1,12 +1,9 @@
 //Modifique la función “greet” para poder implementar una función como parámetro la cual se encargará de imprimir o generar el saludo, establezca los tipos de datos necesarios para que la función cumpla con los requerimientos de TypeScript
-const greet = (nombre : string, edad: number, cb: (saludo: string) => string) => {
-    let saludo = ('Hello '+ nombre + ' your age is '+ edad);
-    return cb (saludo);
- }
-  
- let never = greet ('Angie', 23, (saludo) => {
+var greet = function (nombre, edad, cb) {
+    var saludo = ('Hello ' + nombre + ' your age is ' + edad);
+    return cb(saludo);
+};
+var never = greet('Angie', 23, function (saludo) {
     return saludo;
- })
-  
- console.log(never);
- 
+});
+console.log(never);
