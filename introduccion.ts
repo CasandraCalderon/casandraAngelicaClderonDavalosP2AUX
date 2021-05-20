fba88e047f23ecb3c8e8b0cd438bc90724e898de
casandra <angie200896@gmail.com>
2021-05-19 20:00:10 -0400

    INTRODUCCION p1

---
+++ introduccion.ts
@@ -0,0 +1,6 @@
+/*var nombre = 'casandra';
+nombre = 123; */
+
+//¿Por qué TypeScript no le permite realizar la acción anterior?
+//R. Porque en typescript al declarar la variable como un String no se la puede manejar como un number a diferencia de javascript
+
